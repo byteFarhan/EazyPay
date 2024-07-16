7afb16dd99680b5f1ad8464b40b51f1094d3d99c
farhan-mazumder <farhanmazumder75@gmail.com>
2024-07-16 16:38:31 +0600

    Register page design part has been added.

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Dashbord from "../Pages/Dashbord/Dashbord";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Register from "../Pages/Register/Register";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Dashbord />,
     // children:
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
     errorElement: <ErrorPage />,
   },
 ]);
